9cf70cc8fb0666712544d9ec834cf6059d4af355
Jacob Bovee (from Dev Box) <jabovee@microsoft.com>
2024-11-15 14:29:18 -0800

    Update benchmarks

--- tests/Benchmarks/Program.cs
+++ tests/Benchmarks/Program.cs
@@ -8,8 +8,6 @@ public class Program
     public static void Main(string[] args)
     {
         var config = ManualConfig.Create(DefaultConfig.Instance).WithArtifactsPath("artifacts").WithOptions(ConfigOptions.JoinSummary);
-        // var reviewValidationSummary = BenchmarkRunner.Run<ReviewValidationBenchmark>(config);
-        var stringValidationBenchmark = BenchmarkRunner.Run<StringValidationBenchmark>(config);
-
+        var reviewValidationSummary = BenchmarkRunner.Run<ReviewValidationBenchmark>(config);
     }
 }
